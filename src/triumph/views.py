64e3c6d6bf09ce30ea853a60d909caed952aa80e
NikolayBorovenskiy <nikolay.borovenskiy@gmail.com>
2017-08-30 18:57:06 +0300

    Fixed problem when there is no emtpy photo for slider

--- src/triumph/views.py
+++ src/triumph/views.py
@@ -6,27 +6,27 @@ from school.models import School
 from galleries.models import Photo
 
 
-def generate_slider_photos(photo_list, empty_photo):
+def generate_slider_photos(photo_list, empty_photo=None):
     for photo in photo_list:
         yield photo
-        yield empty_photo
+        if empty_photo:
+            yield empty_photo
 
 
 def home(request):
     school_queryset = School.objects.all()
     slider_photo_qs = Photo.objects.filter(is_slider_photo=True).order_by('?')
-    #order_by('?') - randomize
+    # order_by('?') - randomize
     empty_photo = Photo.objects.filter(name='empty')
-    
-    slider_photos = tuple(
-        generate_slider_photos(slider_photo_qs, empty_photo[0]))
+    slider_photos = generate_slider_photos(slider_photo_qs, empty_photo[0]) if \
+        len(empty_photo) else generate_slider_photos(slider_photo_qs)
     if school_queryset:
         instance = school_queryset[0]
     else:
         instance = None
     context = {
         'instance': instance,
-        'slider_photos': slider_photos,
+        'slider_photos': tuple(slider_photos),
         'title': u'Главная'
     }
     return render(request, 'home.html', context)
