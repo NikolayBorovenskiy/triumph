e2889d9bc83f94591fad71b010b2a5e631ef9001
NikolayBorovenskiy <nikolay.borovenskiy@gmail.com>
2018-09-03 18:44:04 +0300

    Adapted style menu for mobiles. Strict mode

--- assets/js/subMenu.js
+++ assets/js/subMenu.js
@@ -1,11 +1,11 @@
 "use strict";
 
 (function($) {
-    if ($(window).width() < 993) {
+    // if ($(window).width() < 993) {
         $('.dropdown-submenu a.dropdown-toggle').on("click", function(e) {
             $(this).next('ul').toggle();
             e.stopPropagation();
             e.preventDefault();
         });
-    }
+    // }
 }(window.jQuery));
